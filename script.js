1e5f9cc58997841a5099ed6d04ac9b2fd1ec3673
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-03 16:53:53 +0200

    Write the single round logic and fix description
    Fix the game rules to play until one of the players reaches 5 points.
    
    Write the pseudocode of the single round logic step and then code it.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 1. Create the game "Rock, Paper, Scissors".
     1.1. It will be a console game (no GUI).
-    1.2. The game will consist of a best of 5 rounds game between a user and the machine.
+    1.2. The game will consist of a game in which the first to reach 5 points between a user and the machine wins.
     1.3. The inputs that the program will have will be a string entered by a user through the keyboard.
     1.4. The outputs are going to be:
         1.4.1. A message of presentation of the game.
@@ -17,7 +17,7 @@
     2.4. Compare the choices of both.
     2.5. Display a console message about who has won the round.
     2.6. Display the current overall score of the game.
-    2.7. Repeat the above steps 4 more times.
+    2.7. Repeat the above steps until one of the players reaches 5 points.
     2.8. Display a final message about who has won the game.
 
 3. Steps to follow:
@@ -26,7 +26,7 @@
   3.2. Write the logic for the user's choice.
   3.3. Declare the variables of the opponents' scoreboard.
   3.4. Write the logic for a single-round game.
-  3.5. Write the logic for the full game (5 rounds).
+  3.5. Write the logic for the full game.
 
 */
 
@@ -68,9 +68,49 @@ function getHumanChoice() {
   return prompt("What's your choice?");
 }
 
+
 // Step 3: PLAYERS SCORE VARIABLES
 
 // Declare two variables named "humanScore" and "computerScore" in the global scope to keep track of the players score.
 // Initialize them with the value of 0.
 let humanScore = 0;
 let computerScore = 0;
+
+
+// Step 4: SINGLE ROUND LOGIC
+
+// Create a new function named "playRound".
+// Define two parameters for "playRound": "humanChoice" and "computerChoice". Use these two parameters to take the human and computer choices as arguments.
+function playRound(computerChoice, humanChoice) {
+
+// Make this function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+  humanChoice.toLowerCase();
+
+// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+  // Make a condition for the following cases:
+    // If the user choice isn't valid (computer wins).
+    // If the user and computer tie.
+    // If the user wins.
+    // If the computer wins.
+// Increment the humanScore or computerScore variable based on the round winner.
+  if (
+    humanChoice !== "rock" && 
+    humanChoice !== "paper" && 
+    humanChoice !== "scissors"
+  ) {
+    ++computerScore;
+    return(console.log(`You lose! ${humanChoice} isn't a valid choice.`));
+  } else if (humanChoice === computerChoice){
+    return(console.log(`It's a tie!`));
+  } else if (
+    ((humanChoice === "rock") && (computerChoice === "scissors")) ||
+    ((humanChoice === "paper") && (computerChoice === "rock")) ||
+    ((humanChoice === "scissors") && (computerChoice === "paper"))
+  ) {
+    ++humanScore;
+    return(console.log(`You win! ${humanChoice} beats ${computerChoice}.`));
+  } else {
+    ++computerScore;
+    return(console.log(`You lose! ${computerChoice} beats ${humanChoice}.`));
+  }
+}
