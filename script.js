a276bbc52753f26c0ec4257cbe41fdceaf1ada3d
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-03 13:55:43 +0200

    Write general description and pseudocode of game

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+/*
+1. Create the game "Rock, Paper, Scissors".
+    1.1. It will be a console game (no GUI).
+    1.2. The game will consist of a best of 5 rounds game between a user and the machine.
+    1.3. The inputs that the program will have will be a string entered by a user through the keyboard.
+    1.4. The outputs are going to be:
+        1.4.1. A message of presentation of the game.
+        1.4.2. A message for the user to enter his choice.
+        1.4.3. A message with the result of the round.
+        1.4.4. A message with the current overall score.
+        1.4.5. A message with the overall winner.
+
+2. The general operation of the game will be:
+    2.1. Display a game presentation message on the console.
+    2.2. Display an input for the user to enter his choice.
+    2.3. The machine makes its own choice.
+    2.4. Compare the choices of both.
+    2.5. Display a console message about who has won the round.
+    2.6. Display the current overall score of the game.
+    2.7. Repeat the above steps 4 more times.
+    2.8. Display a final message about who has won the game.
+
+3. Steps to follow:
+  
+  3.1. Write the logic for the machine's choice.
+  3.2. Write the logic for the user's choice.
+  3.3. Declare the variables of the opponents' scoreboard.
+  3.4. Write the logic for a single-round game.
+  3.5. Write the logic for the full game (5 rounds).
+
+*/
