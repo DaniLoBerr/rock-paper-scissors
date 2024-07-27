6e56ee5abcf6fa1be738f04fd1dd069c1d17b8ad
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-27 14:11:41 +0200

    Add more game UI elements

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function playGame() {
   const gameContainer = document.querySelector("#game-container");
   const choiceQuestion = document.querySelector("#choice-question");
 
+  // Choice buttons
   const rockButton = document.createElement("button");
   rockButton.classList.add("choice-button");
   rockButton.setAttribute("id", "rock");
@@ -24,6 +25,23 @@ function playGame() {
   gameContainer.appendChild(rockButton);
   gameContainer.appendChild(paperButton);
   gameContainer.appendChild(scissorsButton);
+
+  // Scoreboard
+  const scoreboard = document.createElement("p");
+  let userScore = 0;
+  let computerScore = 0;
+  scoreboard.textContent = `User ${userScore} - ${computerScore} Computer`;
+  gameContainer.appendChild(scoreboard);
+
+  // Round result message
+  const roundResultMessage = document.createElement("p");
+  roundResultMessage.textContent = "";
+  gameContainer.appendChild(roundResultMessage);
+
+  // Game result message
+  const gameResultMessage = document.createElement("p");
+  gameResultMessage.textContent = "";
+  gameContainer.appendChild(gameResultMessage);
 
 }
 
